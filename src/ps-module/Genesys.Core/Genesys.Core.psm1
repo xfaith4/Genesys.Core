d2d83aa2298b042bbc12b288c9cac6ea3d8fe80c
benjaminfuhr <ben.fuhr@gmail.com>
2026-03-05 07:47:16 -0500

    Remove deprecated Genesys.Core GUI and associated modules
    - Deleted GenesysCore-GUI.ps1, which provided a WPF GUI for dataset execution with OAuth authentication.
    - Removed GenesysOps.psd1 and GenesysOps.psm1 as they served as deprecated back-compatibility shims.
    - Eliminated Genesys.Core.psd1 and Genesys.Core.psm1, which were also deprecated shim paths.
    - Removed the legacy Invoke-Dataset.ps1 script, redirecting users to the new module paths.

--- src/ps-module/Genesys.Core/Genesys.Core.psm1
+++
@@ -1,8 +0,0 @@
-### BEGIN: LegacyShim
-Write-Warning '[Genesys.Core] src/ps-module path is deprecated. Use ./modules/Genesys.Core/Genesys.Core.psd1'
-$modulePath = Join-Path $PSScriptRoot '../../../modules/Genesys.Core/Genesys.Core.psd1'
-if (-not (Test-Path $modulePath)) {
-    throw "Genesys.Core module not found at '$modulePath'."
-}
-Import-Module $modulePath -Global -Force -ErrorAction Stop
-### END: LegacyShim
